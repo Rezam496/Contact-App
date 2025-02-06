825df5cf92a95cbfc1f1b8b3fe23403b3844a105
Reza Mohammadi <reza.mohammadi9727@gmail.com>
2025-02-06 18:50:56 +0330

    Create Header project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+function Header() {
+  return (
+    <div className={styles.container}>
+        <h1>Contact App</h1>
+        <p>React.js Full Course</p>
+    </div>
+  )
+}
+
+export default Header
